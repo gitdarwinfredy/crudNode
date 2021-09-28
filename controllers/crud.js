aeefd8e39c1ecd159d8e3fd9ccacde2d8d64bf35
gitdarwinfredy <agarcia1007@une.net.co>
2021-09-27 21:35:42 -0500

    editando crudd

--- controllers/crud.js
+++ controllers/crud.js
@@ -16,4 +16,15 @@ exports.save = (req, res)=>{
     )
 }
 
+exports.update = (req, res)=>{
+    const id = req.body.id;
+    const user = req.body.user;
+    const rol = req.body.rol;
+    conexion.query('UPDATE users SET ? WHERE id=?', [{rol:rol, user:user}, id], (error,results)=>{
+        if (error){
+            console.log(error);
+            }else{
+                res.redirect('/');
+            }
+    })}
 
